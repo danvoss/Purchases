134f4bee5ee5c82b602ba39bb6bcaf0adecc8c55
Dan <danielandrewvoss@gmail.com>
2016-06-22 12:50:17 -0400

    Initial import

---
+++ src/main/java/com/dvoss/PurchasesApplication.java
@@ -0,0 +1,12 @@
+package com.dvoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchasesApplication.class, args);
+	}
+}
